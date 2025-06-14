23793cfb1f5d5680e946ce94e64ac5b812a99044
olgatenison <88651744+olgatenison@users.noreply.github.com>
2025-06-14 14:13:11 +0300

    vorm

--- js/form.js
+++ js/form.js
@@ -1,6 +1,6 @@
 /* -------------------- НАЛАШТУЙ -------------------- */
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbzOMuwkLePSfI1jeOoqx-nKVUaNr33vVBRewFba2XnZzUblv548Dj5SfbjezQgOcp5N/exec";
+  "https://script.google.com/macros/s/AKfycbycSSNA99DkGCWlRpF2icbI-2t5zay4xzFHQPm-Hrk7dt-oEoUZIOjqW0OusxeQhQ6z/exec";
 
 /* -------------------------------------------------- */
 
