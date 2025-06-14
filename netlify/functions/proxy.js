1478243f9eb676c61d00394e176b5c5edcd26335
olgatenison <88651744+olgatenison@users.noreply.github.com>
2025-06-14 14:53:11 +0300

    inport

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch"); // або import, якщо модулі
+import fetch from "node-fetch";пшеimport fetch from 'node-fetch';
 
 exports.handler = async function (event, context) {
   const scriptUrl = "https://script.google.com/macros/s/ТУТ_ID/exec";
